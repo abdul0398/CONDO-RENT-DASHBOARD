1daa4a3e2fe5052d2bab852cd5042b986579cc09
abdul0398 <abdulrkhan570@gmail.com>
2024-04-07 16:02:50 +0530

    Merged datas

--- script.js
+++ script.js
@@ -4,18 +4,37 @@ const data2 = require("./src/data/rentals2.json");
 const data3 = require("./src/data/rentals3.json");
 const data4 = require("./src/data/rentals4.json");
 
-function modifyData(data) {
-  const result = [...data1, ...data2, ...data3, ...data4];
-  const set = new Set();  
+function mergeAndAverageRent(data) {
+  console.log(data.length);
+  const mergedData = [];
+  const dataMap = new Map();
+  let flag = 0;
+  
+ 
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index]
+     // Create a key without the rent property
+     const key = JSON.stringify({ ...item, rent: undefined });
+ 
+     if (dataMap.has(key)) {
+       // If the object already exists in the map, calculate the average rent
+       const existingItem = dataMap.get(key);
+       const averageRent = (existingItem.rent + item.rent) / 2;
+       // Merge the objects, excluding the rent property
+       const mergedItem = { ...existingItem, rent: averageRent };
+       mergedData.push(mergedItem);
+       dataMap.delete(key); // Remove the existing item from the map
+     } else {
+       // If the object does not exist in the map, add it
+       dataMap.set(key, item);
+     }
+  };
+ 
+  // Add any remaining items from the map to the mergedData array
+  dataMap.forEach(item => mergedData.push(item));
+  console.log(mergedData.length);
+  fs.writeFileSync("src/data/Rental4.json", JSON.stringify(mergedData, null, 2));
+ }
+ 
 
-
-  result.forEach((item) => {
-    const month = `20${item.leaseDate.slice(2)}-${item.leaseDate.slice(0, 2)}`;
-    set.add(item.areaSqft);
-
-  })
-
-console.log(set);
-}
-
-modifyData(data4);
+ mergeAndAverageRent(data4);
